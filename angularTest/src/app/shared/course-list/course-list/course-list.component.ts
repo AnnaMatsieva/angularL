49f3cabb230161ffd4fa5b998bb70f491d635e54
Anna Matieva <anna_matieva@epam.com>
2019-10-18 16:23:19 +0300

    markup

--- angularTest/src/app/shared/course-list/course-list/course-list.component.ts
+++ angularTest/src/app/shared/course-list/course-list/course-list.component.ts
@@ -10,14 +10,14 @@ export class CourseListComponent implements OnInit {
   public courseItems: CourseListItem[] = [
     {
       id: 1,
-      title: 'Item 1',
+      title: 'Video Course 1. Name tag',
       time: '1h 28min',
       date: '9Nov, 2019',
       description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
     },
     {
       id: 2,
-      title: 'Item 2',
+      title: 'Video Course 2. Name tag',
       time: '1h',
       date: '15Nov, 2019',
       description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester."
